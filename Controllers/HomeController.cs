5a69849d72fd4cedfbfa55d05e3fef49e761d35d
Leah Rapp <leah.rapp@gmail.com>
2018-11-10 10:10:01 -0500

    boop

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using registration.Models;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RazorRegister()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult RazorRegister(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                ViewBag.Success = "Form Submitted";
+            }
+            return View(model);
+
+        }
+
+        public ActionResult HtmlRegister()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult HtmlRegister(string FirstName, string LastName)
+        {
+
+            var firstName = FirstName;
+            ViewBag.Success = "Awesome";
+    //anything else 
+         return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult HtmlSubmitRequest()
+        {
+            var firstName = Request["FirstName"];
+            
+           //do some validation or logic 
+
+            return RedirectToAction("Success");
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult HtmlSubmit(RegisterViewModel register)
+        {
+         
+            if (!ModelState.IsValid)
+            {
+
+                RedirectToAction("Error");
+            }
+
+           
+
+            return RedirectToAction("Success"); 
+        }
+
+
+        public ActionResult Error()
+        {
+
+
+
+            return View(); 
+        }
+
+
+        public ActionResult Success()
+        {
+
+
+
+            return View(); 
+        }
+
+
+    }
+}
---
+++ JavaScript.js
@@ -0,0 +1,31 @@
+﻿$(function() {
+
+    $("#LastName").change(function() {
+        var val = $("#LastName").val();
+        debugger 
+         val = isValid(val);
+
+        if (!val) {
+
+            $(this).addClass("notValid");
+
+        } else {
+
+
+            $(this).remove("class", "notValid");
+
+
+        }
+
+
+    });
+
+
+});
+
+function isValid(val) {
+
+
+    return false;
+
+}
